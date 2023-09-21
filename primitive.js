bbffbb933481f871f438c2ed89668065b61b1ed2
roag03 <agraharidurgesh09@gmail.com>
2023-09-21 18:35:43 +0530

    day4

---
+++ primitive.js
@@ -0,0 +1,18 @@
+let a=null;
+let b=675;
+ let c=true; //can also be false
+ let d=BigInt(342) + BigInt(8)
+ let e=Symbol("Rohit has very good friens")
+ let f="ROhit has very good family"
+ let g
+ console.log(a,b,c,d,e,f,g)
+ console.log(typeof d)
+
+//  non premitive data type also called object in JavaScript
+const item ={
+    "Rohit" :1,
+    "Yash " :true,
+   "pranjal" :false,
+    "cllg" :undefined
+}
+console.log(item["Rohit"])
