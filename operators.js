d3df3a134efdd19e551cf4b7f8ba5cf4bd89ed25
roag03 <agraharidurgesh09@gmail.com>
2023-09-22 07:48:22 +0530

    javabootcamp

---
+++ operators.js
@@ -0,0 +1,37 @@
+a=54
+b=3
+
+// arithmetic operators
+
+console.log("a+b is", a+b)
+console.log("a-b is" , a-b)
+console.log("a/b is"  ,a/b)
+console.log("a*b is" , a*b)
+console.log("a**b is" , a**b)
+console.log("a**b is" ,a%b)
+console.log("a++ is" ,a++)
+console.log("++a is" ,++a)
+console.log("a-- is" ,a--)
+console.log("--a , is" ,--a)
+
+// assignment operators
+let ass =5;  
+ ass +=6;              // same as ass=ass+6 
+console.log(ass)
+
+// comparison operator
+let comp1=7;
+let comp2="7";
+console.log("comp1==comp2 is",comp1==comp2)
+console.log("comp1!=comp2 is" ,comp1!=comp2)
+console.log("comp1!==comp2 is" ,comp1!==comp2)
+console.log("comp1===comp2 is" ,comp1===comp2)
+
+// logical operator
+
+ let x=5;
+ let y=8;
+ console.log("x<y&&x==5 is" ,x<y&&x==5)
+ console.log("x>y||x==5 is" ,x>y||x==5)
+ console.log(!true)
+ console.log(!false)
---
+++ practice.js
@@ -0,0 +1,27 @@
+// Problem No 1
+let a="Rohit";
+let b=7;
+console.log((a+b));
+
+// problem no 2
+
+console.log(typeof(a+b))
+
+// problem no 3
+const a1 = {
+    name :"ROhit",
+    section :1,
+    Friend: false
+}
+a1['friend']="Yash trehan"
+a1['name']="Rohit"
+console.log(a1)
+
+// problem no 5
+const dict = {
+    debuff : "to decrease a character",
+    selcouth:"strange",
+    yakka :"hard work",
+    ataraxia:"state of freedom feom emotional desturbance and anxiety"
+}
+console.log(dict.yakka)
