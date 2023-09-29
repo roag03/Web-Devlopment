722524241481e4d80e33c5f51edebddff14aabb5
roag03 <agraharidurgesh09@gmail.com>
2023-09-29 20:28:28 +0530

    java

--- practice2.js
+++ practice2.js
@@ -1,8 +1,20 @@
-let a= prompt("what is you age??")
+/* let a= prompt("what is you age??")
 if(a>10 && a<20){
     alert("you are lies between 10 & 20")
     
 }
 else{
     (alert("you are not lies between 10 & 20"))
+} */
+let age =prompt("what is your age");
+switch(age){
+    case '12':
+        console.log("you are  12 year old")
+        
+        case '13':
+            console.log("your age is 13 year ")
+          
+            case '14':
+                console.log("you are 14 year old")
+                break
 }
