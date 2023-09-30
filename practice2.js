62259f4fa07441d5828c207dc36b6c381022532b
roag03 <agraharidurgesh09@gmail.com>
2023-09-30 23:45:48 +0530

    js prblm

--- practice2.js
+++ practice2.js
@@ -1,4 +1,5 @@
 /* let a= prompt("what is you age??")
+age= Number.parseInt(age)
 if(a>10 && a<20){
     alert("you are lies between 10 & 20")
     
@@ -6,7 +7,8 @@ if(a>10 && a<20){
 else{
     (alert("you are not lies between 10 & 20"))
 } */
-let age =prompt("what is your age");
+/* let age =prompt("what is your age");
+
 switch(age){
     case '12':
         console.log("you are  12 year old")
@@ -18,3 +20,30 @@ switch(age){
                 console.log("you are 14 year old")
                 break
 }
+*/
+// pronlem 3
+// let num=prompt("what is your age?");
+// num =Number.parseInt(num)
+// if (num %2 ==0 && num % 3 ==0){
+//     console.log("your number is divisible by 2 and 3")
+// }
+// else{
+//     console.log("your number is not divisible by 2 and 3")
+// }
+// problem no 4
+
+// let num =prompt("what is your number");
+// num=Number.parseInt(num)
+// if (num%2==0 || num%3==0){
+//     console.log("your number is divisible by 2 or 3")
+
+// }
+// else{
+//     console.log("your number is not dividible by 2 or 3")
+// }
+
+// proble number 5
+
+let age=19
+let a=age> 18? "you can drive": "you cannot drive"
+console.log(a)
